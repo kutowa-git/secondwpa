5c409dfcff066b4fc602b138ec2deb3ce3f0b162
kuwae_toshiki <kutowashi12@gmail.com>
2025-06-15 13:58:37 +0900

    first commit

--- service_worker.js
+++ service_worker.js
@@ -1,15 +1,11 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('pwa-cache').then(cache => {
-      return cache.addAll(['/', '/index.html', '/style.css']);
-    })
-  );
-});
+function updateClock() {
+  const clockElem = document.getElementById('clock');
+  const now = new Date();
+  // 時刻を「hh:mm:ss」の形式で作成
+  const timeString = now.toLocaleTimeString();
+  clockElem.textContent = timeString;
+}
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// ページ読み込み時と1秒ごとに時計を更新
+updateClock();
+setInterval(updateClock, 1000);
